d7591b974b485f6606b77682e463c7de73ed590f
Uche <68336838+ucheorjins@users.noreply.github.com>
2025-07-17 12:44:16 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ st.title("Top 300 Movie of All Time (Maybe)")
 
 # Safely convert datatypes
 df["year"] = df["year"].astype(str).str.extract(r"(\d{4})").astype(float)
-df["RT rating"] = pd.to_numeric(df["RT rating"], errors="coerce")
+df["RT rating (%)"] = pd.to_numeric(df["RT rating (%)"], errors="coerce")
 df["imdb rating"] = pd.to_numeric(df["imdb rating"], errors="coerce")
-df["runtime"] = pd.to_numeric(df["runtime"], errors="coerce")
+df["runtime (minutes)"] = pd.to_numeric(df["runtime (minutes)"], errors="coerce")
 df["genre"] = df["genre"].fillna("")
 
 # Filter sidebar
@@ -27,7 +27,7 @@ with st.sidebar.form("filter_form"):
     
     rt_min, rt_max = st.slider("RT Rating %", 0, 100, value=(0, 100))
     imdb_min, imdb_max = st.slider("IMDb Rating", 0.0, 10.0, value=(0.0, 10.0))
-    runtime_min, runtime_max = st.slider("Runtime (min)", 0, int(df["runtime"].max()), value=(0, int(df["runtime"].max())))
+    runtime_min, runtime_max = st.slider("Runtime (min)", 0, int(df["runtime (minutes)"].max()), value=(0, int(df["runtime (minutes)"].max())))
 
     col1, col2 = st.columns([1, 1])
     search_btn = col1.form_submit_button("🔍 Search")
@@ -51,9 +51,9 @@ filtered = df.copy()
 
 # --Fill missing values so comparisons don’t break--
 filtered["year"] = filtered["year"].fillna(0)
-filtered["RT rating"] = filtered["RT rating"].fillna(0)
+filtered["RT rating (%)"] = filtered["RT rating (%)"].fillna(0)
 filtered["imdb rating"] = filtered["imdb rating"].fillna(0)
-filtered["runtime"] = filtered["runtime"].fillna(0)
+filtered["runtime (minutes)"] = filtered["runtime (minutes)"].fillna(0)
 filtered["genre"] = filtered["genre"].fillna("")
 
 # --Title search (optional)--
@@ -67,9 +67,9 @@ if genre_query:
 # --Numeric filters (always apply, using sliders)--
 filtered = filtered[
     (filtered["year"] >= year_min) & (filtered["year"] <= year_max) &
-    (filtered["RT rating"] >= rt_min) & (filtered["RT rating"] <= rt_max) &
+    (filtered["RT rating (%)"] >= rt_min) & (filtered["RT rating (%)"] <= rt_max) &
     (filtered["imdb rating"] >= imdb_min) & (filtered["imdb rating"] <= imdb_max) &
-    (filtered["runtime"] >= runtime_min) & (filtered["runtime"] <= runtime_max)
+    (filtered["runtime (minutes)"] >= runtime_min) & (filtered["runtime (minutes)"] <= runtime_max)
 ]
 
 #To show all movies 
